7587c3adb6f8e17ebbdc5d0830abf695ff89beaf
Yifan Cheng <yifan.cheng@xtreamcompute.com>
2023-02-02 16:55:18 +1100

    initial commit

---
+++ cplusplus_com_trials/basics_of_cpp.cpp
@@ -0,0 +1,69 @@
+// to compile this file, type in terminal: g++ -std=c++20 basics_of_cpp.cpp -o basics_of_cpp
+// to run the compiled binary, type in terminal: ./basics_of_cpp
+ 
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+/** @brief Print the version of the current c++ standard of the compiler in the command line.*/
+void cpp_standard_ver_trial() {
+    if (__cplusplus == 202002L) std::cout << "**C++20\n";
+    else if (__cplusplus == 201703L) std::cout << "**C++17\n";
+    else if (__cplusplus == 201402L) std::cout << "**C++14\n";
+    else if (__cplusplus == 201103L) std::cout << "**C++11\n";
+    else if (__cplusplus == 199711L) std::cout << "**C++98\n";
+    else std::cout << "pre-standard C++\n";
+}
+
+/** @brief This feature is specific to c++20 or plus. Older standard, such as c++17, will show error for this function.*/
+void feature_of_cpp20_trial() {
+  auto result = (10 <=> 20) > 0;
+  cout << "result: " << result << std::endl;
+}
+
+/** @brief Print out the c++ types.*/
+void cpp_types_trial() {
+  string output  = "";
+  
+  { // string(1, var) converts a char to a string
+    char var = 5; // this assigned the char with ascii code 0
+    output += "var: " + string(1, var) + ", type: " + typeid(var).name() + ", size of var: " + to_string(sizeof(var)) + "\n";
+  }
+  {
+    char var = '5';
+    output += "var: " + string(1, var) + ", type: " + typeid(var).name() + ", size of var: " + to_string(sizeof(var)) + "\n";
+  }
+  { // to_string only converts an int to a string
+    char var = 5;
+    output += "var: " + to_string(var) + ", type: " + typeid(var).name() + ", size of var: " + to_string(sizeof(var)) + "\n";
+  }
+  {
+    char var = '5';
+    output += "var: " + to_string(var) + ", type: " + typeid(var).name() + ", size of var: " + to_string(sizeof(var)) + "\n";
+  }
+  {
+    char16_t var = '5';
+    output += "var: " + string(1, var) + ", type: " + typeid(var).name() + ", size of var: " + to_string(sizeof(var)) + "\n";
+  }
+  {
+    char32_t var = '5';
+    output += "var: " + string(1, var) + ", type: " + typeid(var).name() + ", size of var: " + to_string(sizeof(var)) + "\n";
+  }
+  {
+    wchar_t var = '5';
+    output += "var: " + string(1, var) + ", type: " + typeid(var).name() + ", size of var: " + to_string(sizeof(var)) + "\n";
+  }
+  {
+    signed char var = '5';
+    output += "var: " + to_string(var) + ", type: " + typeid(var).name() + ", size of var: " + to_string(sizeof(var)) + "\n";
+  }
+
+  cout << output;
+}
+
+int main() {
+    cpp_types_trial();
+    return 0;
+}
