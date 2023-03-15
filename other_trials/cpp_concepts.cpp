b3d22a3a998d0d2e7df30096c483cb86583e61e5
Yifan Cheng <yifan.cheng@anu.edu.au>
2023-03-15 22:25:20 +1100

    add more printings to cpp_concepts

--- other_trials/cpp_concepts.cpp
+++ other_trials/cpp_concepts.cpp
@@ -6,14 +6,25 @@ to run the compiled binary, type in terminal in the root folder:
 */
 
 #include <iostream>
+#include <typeinfo>
 
 using namespace std;
 
 /** @brief trial of std::integral_constant */
-template <int n> struct twice_cons : integral_constant<int, n*2> {};
+template <int n> struct twice_cons : integral_constant<int, n*2> { inline static int triple_v = 100; };
 void integral_constant_trial() {
   twice_cons<7> twice_v;
+  cout << twice_cons<7>::triple_v << endl;
   cout << twice_v.value << endl;
+  cout << typeid(twice_cons<7>::type).name() << endl;
+  cout << twice_cons<7>::value;
+  cout << typeid(twice_cons<7>::value_type).name() << endl;
+  twice_cons<7>::triple_v = 101;
+  cout << twice_cons<7>::triple_v << endl;
+  twice_v.triple_v = 102;
+  cout << twice_cons<7>::triple_v << endl;
+  cout << twice_cons<8>::triple_v << endl;
+  cout << twice_v.triple_v << endl;
 }
 
 int main() {
