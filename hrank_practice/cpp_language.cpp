9e3cb4e01d4d998584c2e81188ad3ecc163ac66a
Yifan Cheng <yifan.cheng@xtreamcompute.com>
2023-04-07 15:49:41 +1000

    add scan_and_printf cpp lang trial

--- hrank_practice/cpp_language.cpp
+++ hrank_practice/cpp_language.cpp
@@ -2,10 +2,31 @@
 to compile this file, type in terminal in the root folder:
   g++ -std=c++20 hrank_practice/cpp_language.cpp -o .bin/hrank_practice@@cpp_language.bin
 to run the compiled binary, type in terminal in the root folder:
-  hrank_practice@@cpp_language.bin
+  .bin/hrank_practice@@cpp_language.bin
 */
 
+#include <iostream>
+using namespace std;
 
-void main() {
+// @brief - scanf and formatted printing
+// @headerfile - #include <iostream>
+void scan_and_printf() {
+    // Complete the code.
+    int intt;
+    long longg;
+    char charr;
+    float floatt;
+    double doublee;
+    scanf("%d %ld %c %f %lf", &intt, &longg, &charr, &floatt, &doublee);
+    printf("%d\n", intt);
+    printf("%ld\n", longg);
+    printf("%c\n", charr);
+    printf("%.3f\n", floatt);
+    printf("%.9lf\n", doublee);
+    return;
+}
 
+int main() {
+ scan_and_printf();
+ return 0;
 }
