1680e789a71c5bf1f2864e8145fe158c07243ffa
Yifan Cheng <yifan.cheng@xtreamcompute.com>
2023-04-07 21:38:21 +1000

    add codility question file

---
+++ other_trials/codility.cpp
@@ -0,0 +1,17 @@
+/*
+to compile this file, type in terminal in the root folder:
+  g++ -std=c++20 other_trials/codility.cpp -o .bin/other_trials@@codility.bin
+to run the compiled binary, type in terminal in the root folder:
+  .bin/other_trials@@codility.bin
+*/
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> &A) {
+    // Implement your solution here
+}
+
+int main() {
+  return 0;
+}
