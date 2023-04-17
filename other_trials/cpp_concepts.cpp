0ade95ab4f135d094512d57d226360d068bfae85
Yifan Cheng <yifan.cheng@xtreamcompute.com>
2023-04-17 21:26:35 +1000

    add vector erase trial

--- other_trials/cpp_concepts.cpp
+++ other_trials/cpp_concepts.cpp
@@ -7,8 +7,46 @@ to run the compiled binary, type in terminal in the root folder:
 
 #include <iostream>
 #include <typeinfo>
+#include <vector>
 
 using namespace std;
+
+/** @brief trial of vector erase */
+void vector_erase_trial() {
+  /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+  int n;
+  cin >> n;
+  vector<int> nums(n, 0);
+  for (int i = 0; i < n; ++i) {
+      cin >> nums[i];
+  }
+  int rm_sg, rm_l, rm_r;
+  cin >> rm_sg >> rm_l >> rm_r;
+  nums.erase(nums.begin()+rm_sg-1);
+
+  cout << "after 1st change: " << endl;
+  for (int i = 0; i < n - 1; ++i) {
+      cout << nums[i] << " ";
+  }
+  cout << nums[n-1] << endl;
+
+  nums.erase(nums.begin()+rm_l-1, nums.begin()+rm_r-1);
+
+  cout << "after 2nd change: " << endl;
+  for (int i = 0; i < n - 1; ++i) {
+      cout << nums[i] << " ";
+  }
+  cout << nums[n-1] << endl;
+  
+  int nn = nums.size();
+  cout << "final outpu: " << endl;
+  cout << nn << endl;
+  // printing of nums
+  for (int i = 0; i < nn - 1; ++i) {
+      cout << nums[i] << " ";
+  }
+  cout << nums[nn-1] << endl;
+}
 
 /** @brief trial of std::integral_constant */
 template <int n> struct twice_cons : integral_constant<int, n*2> { inline static int triple_v = 100; };
@@ -28,6 +66,6 @@ void integral_constant_trial() {
 }
 
 int main() {
-    integral_constant_trial();
+    vector_erase_trial();
     return 0;
 }
