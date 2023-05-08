cd88834708b104cd656664263bcec6b97b61900b
Yifan Cheng <yifan.cheng@xtreamcompute.com>
2023-05-08 15:47:31 +1000

    and trials for bitwise operations

--- other_trials/cpp_concepts.cpp
+++ other_trials/cpp_concepts.cpp
@@ -105,7 +105,37 @@ void integral_constant_trial() {
   cout << twice_v.triple_v << endl;
 }
 
+
+/** @brief trial of bitwise operations */
+void bitwise_operation_trial() {
+  int a = 60;	/* 60 = 0011 1100 */  
+  int b = 13;	/* 13 = 0000 1101 */
+  int c = 0;           
+
+  c = a & b;       /* 12 = 0000 1100 */ 
+  cout << "Line 1 - Value of c is : " << c << endl ;
+
+  c = a | b;       /* 61 = 0011 1101 */
+  cout << "Line 2 - Value of c is: " << c << endl ;
+
+  c = a ^ b;       /* 49 = 0011 0001 */
+  cout << "Line 3 - Value of c is: " << c << endl ;
+
+  c = ~a;          /*-61 = 1100 0011 */
+  cout << "Line 4 - Value of c is: " << c << endl ;
+
+  c = a << 2;     /* 240 = 1111 0000 */
+  cout << "Line 5 - Value of c is: " << c << endl ;
+
+  c = a >> 2;     /* 15 = 0000 1111 */
+  cout << "Line 6 - Value of c is: " << c << endl ;
+
+  c = a & 1;       /* 0 = 0000 0000 */
+  cout << "Line 7 - Value of c is : " << c << endl ;
+}
+
+
 int main() {
-    list_insert_trial();
+    bitwise_operation_trial();
     return 0;
 }
