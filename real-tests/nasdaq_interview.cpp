d80040e6e4210365e77db20195becc6330a8c2b0
Yifan Cheng <yifan.cheng@xtreamcompute.com>
2023-06-11 21:34:14 +1000

    add nasdaq interview

---
+++ real-tests/nasdaq_interview.cpp
@@ -0,0 +1,53 @@
+/*
+to compile this file, type in terminal in the root folder:
+  g++ -std=c++20 other_trials/tt.cpp -o .bin/other_trials@@tt.bin
+to run the compiled binary, type in terminal in the root folder:
+  .bin/other_trials@@tt.bin
+*/
+
+#include <iostream>
+#include <typeinfo>
+#include <vector>
+#include <list>
+#include <unordered_map>
+
+using namespace std;
+
+// void min_anx_max() {
+//   string line;
+//   string place;
+//   unordered_map<string, pair<int, int>> map;
+//   while (cin) {
+//     cin >> line;
+//     cin >> place;
+//     // string place;
+//     // for (char c: line) {
+//     //   if (c != ' ') {
+//     //     place += c;
+//     //   } else {
+//     //     break;
+//     //   }
+//     // }
+//     double num = (double) line.substr(place.size() + 1);
+//     if (map.count(place) == 0) {
+//       map[place] = make_pair(num, num);
+//     } else {
+//       if (num < map[place].first) {
+//         map[place].first = num;
+//       } else if (num > map[place].second) {
+//         map[place].second = num;
+//       }
+//     }
+//   }
+//   for (const auto & [key, value]: map) {
+//     cout << key << ", " << value.first << ", " << value.second << endl;
+//   }
+// }
+
+
+
+
+int main() {
+    
+    return 0;
+}
